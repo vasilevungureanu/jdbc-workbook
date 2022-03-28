83ee980bc8ebbedc9990710270cf758079d7a33f
Vasile <vasile.v.ungureanu@gmail.com>
2022-03-28 12:03:35 +0300

    Connect to H2 database

---
+++ src/main/java/com/vasileungureanu/Application.java
@@ -0,0 +1,14 @@
+package com.vasileungureanu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Application {
+
+  public static void main(String[] args) throws SQLException {
+    try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:")) {
+      System.out.println("connection.isValid(0) = " + connection.isValid(0));
+    }
+  }
+}
