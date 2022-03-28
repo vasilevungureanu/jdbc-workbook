224b50862173f6d47052fca229f1acea8943ab0c
Vasile <vasile.v.ungureanu@gmail.com>
2022-03-28 12:14:37 +0300

    Connect to File-Based H2 database with wrong credentials

---
+++ src/main/java/com/vasileungureanu/ApplicationV4.java
@@ -0,0 +1,18 @@
+package com.vasileungureanu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ApplicationV4 {
+
+  public static void main(String[] args) throws SQLException {
+    try (Connection connection = DriverManager.getConnection(
+        "jdbc:h2:~/mydatabase",
+        "sa",
+        "definitelySomeOtherPassword"
+    )) {
+      System.out.println("connection.isValid(0) = " + connection.isValid(0));
+    }
+  }
+}
